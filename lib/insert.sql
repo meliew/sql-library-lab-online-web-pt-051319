394666d30de857a0847e73ae20a31f29ac895b9d
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-21 02:10:48 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,8 +4,8 @@ INSERT INTO series (id, title, author_id, subgenre_id) VALUES
 
 INSERT INTO books (id, title, year, series_id) VALUES
 (1, "First Book", 2020, 1),
-(2, "Star Wars: Part 1", 2011, 2);
-(3, "Star Wars: Part 2", 2013, 2);
-(4, "Star Wars: Part 3", 2014, 2);
-(5, "Second Book", 2021, 1);
+(2, "Star Wars: Part 1", 2011, 2),
+(3, "Star Wars: Part 2", 2013, 2),
+(4, "Star Wars: Part 3", 2014, 2),
+(5, "Second Book", 2021, 1),
 (6, "Last Book", 2022, 1);
