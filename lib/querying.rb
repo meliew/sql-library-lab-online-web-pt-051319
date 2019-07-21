101d64bd53a4a8c300c163dcdc246b511b16806b
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-21 02:53:16 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,10 +25,9 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series
-  WHERE series.species "
+  " "
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(characters.books) FROM characters"
 end
