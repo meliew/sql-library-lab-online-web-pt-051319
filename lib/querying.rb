95be1c8e78b88cf4bc7f5e0baa8208bbc39a11e5
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-21 02:36:43 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,7 +11,8 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, "
+  "SELECT characters.species FROM characters 
+  ORDER BY LENGTH(species) DESC LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
