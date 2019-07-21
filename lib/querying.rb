0e7c5385bbe162dca5ab291ce76a4b359b87044f
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-21 03:02:37 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,7 +25,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series 
+  "SELECT series.title FROM series
   LEFT OUTER JOIN characters ON characters.id = series.character_id
   "
 end
