5e27347058bca6f07eedd1cd32d531c5b2ee993f
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-21 20:02:28 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -37,7 +37,7 @@ end
 def select_character_names_and_number_of_books_they_are_in
 
   "SELECT characters.name, COUNT(name) FROM character_books
-  INNER JOIN characters ON characters.id = character_books.character.id
+  INNER JOIN characters ON characters.id = character_books.character_id
   GROUP BY characters.name
   ORDER BY COUNT(name)
   DESC"
