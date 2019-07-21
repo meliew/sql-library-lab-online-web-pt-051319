437987949b7c413e2d4e3131552998c22aabfff2
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-21 02:34:55 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -6,7 +6,7 @@ end
 
 def select_name_and_motto_of_char_with_longest_motto
   "SELECT characters.name, characters.motto FROM characters
-  WHERE characters.motto.length(MAX)"
+  ORDER BY LENGTH(motto) DESC LIMIT 1"
 end
 
 
@@ -19,7 +19,8 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series
+  WHERE series.species "
 end
 
 def select_character_names_and_number_of_books_they_are_in
