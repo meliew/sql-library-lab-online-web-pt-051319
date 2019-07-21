a55e21c62b91fa64e4529d7c4a2d29791c04ffa5
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-21 02:51:07 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,9 +18,9 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, subgenres.name FROM subgenres 
+  "SELECT authors.name, subgenres.name FROM subgenres
   LEFT OUTER JOIN series ON subgenres.id = series.id
-  
+
   "
 end
 
